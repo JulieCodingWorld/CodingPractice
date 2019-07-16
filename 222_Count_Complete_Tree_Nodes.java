28b1604f672363c3954ae4c850e9607ca39dc3de
julileong <julielong165@gmail.com>
2019-07-16 11:49:16 -0400

    julie

--- 222_Count_Complete_Tree_Nodes.java
+++ 222_Count_Complete_Tree_Nodes.java
@@ -9,11 +9,13 @@
  */
 public class Solution {
     public int countNodes(TreeNode root){
+        
         if(root == null){
             return 0;
         }
        else {
            return 1 + countNodes(root.left) + countNodes(root.right);
+           
        }
     }
 }
