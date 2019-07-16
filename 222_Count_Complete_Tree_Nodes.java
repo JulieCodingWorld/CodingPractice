690dbbbd67de3a130516998f1a88f84a8d27ceb9
julileong <julielong165@gmail.com>
2019-07-16 11:26:55 -0400

    222

---
+++ 222_Count_Complete_Tree_Nodes.java
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int countNodes(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+       else {
+           return 1 + countNodes(root.left) + countNodes(root.right);
+       }
+    }
+}
